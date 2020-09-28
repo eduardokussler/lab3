23ed1d9114e4f1c3ea8697f85b2348aac1e10384
Eduardo Kussler <eduardoeugeniok@gmail.com>
2020-09-28 18:36:57 -0300

    Update msd.py

--- msd.py
+++ msd.py
@@ -1,5 +1,5 @@
 class radix_MSD:
-  R = 256
+  R = 25 #26 caracteres, posicao 0 a 25
   M = 15
   aux = ''
   def __init__(self):
@@ -8,7 +8,6 @@ class radix_MSD:
   def insertion_sort(self, C, first, end):
     key = 0
     biggest = C[0]
-    C = list(C)
     i = 0
     j = 0
     for i in range(first+1, end):
@@ -19,20 +18,42 @@ class radix_MSD:
         j -= 1 
       C[j+1] = key
     return "".join(C)
-
+  
+  
+  def counting_sort(self, A, low, hi, d):
+    C = [None] * (self.R +2) 
+    A = list(A)
+    B = [None] * len(A)
+    for i in range(low, hi+1):
+      C[i] = 0
+    for i in range(0, self.R+1):
+      #ord retorna o codigo ascii do char
+      #como só temos 26 chars, alinhando os caracteres
+      # a = indice 0
+      # b = indice 1...
+      C[ord(A[i])-65] += 1
+    for i in range(low, hi+1):
+      B[C[ord(A[i])-65]] = A[i]
+    for j in range(len(A)-1, -1, -1):
+      B[C[ord(A[j])-65]-1] = A[j]
+      C[ord(A[j])-65] -= 1
+    return "".join(B)
+     
   def sort(self, palavras, low, hi, d):
+    palavras = list(palavras)
     if(hi <= low + self.M):
-      insertion_sort(palavras, low, hi)
+      self.insertion_sort(palavras, low, hi)
+      return
+    palavras = self.counting_sort(palavras, low, hi, d)
 
   def radix_sort(self, palavras):
     N = len(palavras)
     #Criando uma lista de Tamanho N vazia
     self.aux = [None] * N
-    sort(palavras, 0, N-1, 0)
+    self.sort(palavras, 0, N-1, 0)
 
 radix = radix_MSD()
-teste = 'wqdaasasahasasasa'
+teste = ['ABACAXI', 'PATETA']
 
-teste = radix.insertion_sort(teste,0,len(teste))
+teste = radix.counting_sort(teste,0, len(teste)-1, 0)
 print(teste)
-
